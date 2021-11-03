3eb051c9428eda35dfadd04e69433742652e47dc
Alex <kolesnikaleksv@gmail.com>
2021-11-03 15:28:25 +0200

    State - practice

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -4,24 +4,13 @@ import './employees-list.css';
 
     const EmployeesList = ({data}) => {
         const elements = data.map(item => {
-//residal destructuring or partial destructurig
-//we can pull the props individually from the item object
             const {id, ...itemProps} = item;
             return(
                 
                 <EmployeesListItem key={id} {...itemProps} />
             )
         })
-//if the element does not change the order in the list we can assign it its number
-    // const EmployeesList = ({data}) => {
-    //     const elements = data.map((item,i) => {
-            
-    //         return(
-                
-    //             <EmployeesListItem key={i} {...item} />
-    //         )
-    //     })
-    // console.log(elements);
+
     return (
         <ul className="app-list list-group">
             {elements}
